e878f581131b04e1e89ffdc91ba72a91b225fc5f
joellastraley <jstraley@gmail.com>
2014-07-25 10:43:41 -0400

    first commit

---
+++ js/router/router.js
@@ -0,0 +1,38 @@
+var PortfolioRouter = Backbone.Router.extend({
+
+  routes: {
+    'home': 'update',
+    'about': 'update',
+    'work': 'update',
+    'contact': 'update',
+    '*default': 'update'
+  },
+
+  update: function() {
+    var id = Backbone.history.fragment || 'home';
+    $('.view').hide().filter('#'+id).show();
+    this.navigate(id);
+  }
+});
+
+
+var NavView = Backbone.View.extend({
+
+  el: '#nav',
+
+  initialize: function(options) {
+    this.listenTo(options.router, 'route', this.render);
+  },
+
+  render: function() {
+    this.$('a')
+      .removeClass('active')
+      .filter('[href="#'+ Backbone.history.fragment +'"]')
+      .addClass('active');
+  }
+});
+
+var portfolioRouter = new PortfolioRouter();
+var navView = new NavView({router: portfolioRouter});
+
+Backbone.history.start();
